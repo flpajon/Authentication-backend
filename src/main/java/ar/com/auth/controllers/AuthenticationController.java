aad30663266584506ebefd05db99fd9ec8069be8
Fran <flpajon@gmail.com>
2022-11-27 14:56:45 -0300

    swagger was implemented

--- src/main/java/ar/com/auth/controllers/AuthenticationController.java
+++ src/main/java/ar/com/auth/controllers/AuthenticationController.java
@@ -2,8 +2,12 @@ package ar.com.auth.controllers;
 
 import ar.com.auth.dtos.requests.SigninRequest;
 import ar.com.auth.dtos.requests.SignupRequest;
+import ar.com.auth.dtos.responses.RefreshTokenResponse;
+import ar.com.auth.dtos.responses.SigninResponse;
+import ar.com.auth.dtos.responses.SignupResponse;
 import ar.com.auth.model.User;
 import ar.com.auth.services.AuthenticationService;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +23,9 @@ public class AuthenticationController {
 
     @Autowired
     private AuthenticationService authenticationService;
-
+    @ApiOperation(value = "This method is used to signup a new user.")
     @PostMapping("signup")
-    public ResponseEntity<?> signUp(@RequestBody SignupRequest signupRequest){
+    public ResponseEntity<SignupResponse> signUp(@RequestBody SignupRequest signupRequest){
         try{
             return ResponseEntity.ok(authenticationService.signupUser(signupRequest));
         }catch (Exception e){
@@ -29,9 +33,9 @@ public class AuthenticationController {
             return ResponseEntity.internalServerError().build();
         }
     }
-
+    @ApiOperation(value = "This method is used to signin a exist user.")
     @PostMapping("signin")
-    public ResponseEntity<?> signIn(@RequestBody SigninRequest signinRequest){
+    public ResponseEntity<SigninResponse> signIn(@RequestBody SigninRequest signinRequest){
         try{
             return ResponseEntity.ok(authenticationService.signinUser(signinRequest));
         }catch (Exception e){
@@ -39,9 +43,9 @@ public class AuthenticationController {
             return ResponseEntity.internalServerError().build();
         }
     }
-
+    @ApiOperation(value = "This method is used to refresh the access token.")
     @GetMapping("refreshToken")
-    public ResponseEntity<?> refreshToken(@AuthenticationPrincipal User user){
+    public ResponseEntity<RefreshTokenResponse> refreshToken(@AuthenticationPrincipal User user){
         try{
             return ResponseEntity.ok(authenticationService.refreshToken(user));
         }catch (Exception e){
--- src/main/java/ar/com/auth/controllers/UserController.java
+++ src/main/java/ar/com/auth/controllers/UserController.java
@@ -1,7 +1,12 @@
 package ar.com.auth.controllers;
 
 import ar.com.auth.dtos.requests.UpdateUserRequest;
+import ar.com.auth.dtos.responses.DisableUserRespose;
+import ar.com.auth.dtos.responses.EnableUserRespose;
+import ar.com.auth.dtos.responses.FetchAllUsersResponse;
+import ar.com.auth.dtos.responses.UpdateUserResponse;
 import ar.com.auth.services.UserService;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +22,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value = "This method is used to return a list of users.")
     @GetMapping("all")
-    public ResponseEntity<?> fetchAllUsers(){
+    public ResponseEntity<FetchAllUsersResponse> fetchAllUsers(){
         try{
             return ResponseEntity.ok(userService.fetchAllUsers());
         }catch (Exception e){
@@ -27,8 +33,9 @@ public class UserController {
         }
     }
 
+    @ApiOperation(value = "This method is used to update a user.")
     @PutMapping("update")
-    public ResponseEntity<?> updateUser(@RequestBody UpdateUserRequest updateUserRequest){
+    public ResponseEntity<UpdateUserResponse> updateUser(@RequestBody UpdateUserRequest updateUserRequest){
         try{
             return ResponseEntity.ok(userService.updateUser(updateUserRequest));
         }catch (Exception e){
@@ -37,8 +44,9 @@ public class UserController {
         }
     }
 
+    @ApiOperation(value = "This method is used to disable a user.")
     @DeleteMapping("disable")
-    public ResponseEntity<?> disableUser(@RequestParam(name = "userName") String userName){
+    public ResponseEntity<DisableUserRespose> disableUser(@RequestParam(name = "userName") String userName){
         try{
             return ResponseEntity.ok(userService.disableUser(userName));
         }catch (Exception e){
@@ -47,8 +55,9 @@ public class UserController {
         }
     }
 
+    @ApiOperation(value = "This method is used to enable a user.")
     @PutMapping("enable")
-    public ResponseEntity<?> enableUser(@RequestParam(name = "userName") String userName){
+    public ResponseEntity<EnableUserRespose> enableUser(@RequestParam(name = "userName") String userName){
         try{
             return ResponseEntity.ok(userService.enableUser(userName));
         }catch (Exception e){
